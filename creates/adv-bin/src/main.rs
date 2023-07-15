d58f559844abc767dd298296ce20ea9037236807
sondq <quocson@gmail.com>
2023-07-15 21:24:03 +0700

    fix

--- creates/adv-bin/src/main.rs
+++ creates/adv-bin/src/main.rs
@@ -2,7 +2,11 @@ use axum::{routing, Router, Server};
 use tracing::info;
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
+    let subsriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subsriber).expect("set global subscriber failed");
     println!("Hello, world!");
     let app = Router::new().route("/", routing::get(hello));
     let addr = "127.0.0.1:1234";
