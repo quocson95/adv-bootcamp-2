991d090f4973a54086e48134a8a843225a40e6af
sondq <quocson@gmail.com>
2023-07-15 21:20:55 +0700

    fix

--- creates/adv-bin/src/main.rs
+++ creates/adv-bin/src/main.rs
@@ -1,10 +1,13 @@
 use axum::{routing, Router, Server};
+use tracing::info;
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     println!("Hello, world!");
     let app = Router::new().route("/", routing::get(hello));
+    let addr = "127.0.0.1:1234";
+    info!("Server is running on addr: {}", &addr);
 
-    let addr = "127.0.0.0.1:8888";
     Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
